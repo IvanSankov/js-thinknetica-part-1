07f83c6066588a27860ad218f08207e74835ad18
ivan san <sii200192@gmail.com>
2020-05-27 15:06:45 +0700

    Доработать графический редактор, начатый на занятии

---
+++ task_5/picture/index.js
@@ -0,0 +1,70 @@
+"use strict";
+
+const settings = {backgroundColor: null};
+let table = document.querySelector('table');
+let input = document.getElementById('input-color');
+
+input.addEventListener('keydown', keydown);
+table.addEventListener('click', click);
+
+fillTable(table);
+setBackgroundColor(input);
+
+/**
+ * Функция устанавливает цвет заливки
+ * @param {HTMLInputElement} input
+ */
+function setBackgroundColor(input) {
+    let target = document.getElementById(input.dataset.target);
+    if (!target) {
+        return;
+    }
+
+    target.style.backgroundColor = input.value;
+    settings.backgroundColor = input.value;
+}
+
+/**
+ * Функция-обработчик нажатие клавиши на поле input
+ * @param {KeyboardEvent} event
+ */
+function keydown(event) {
+    setBackgroundColor(event.target);
+}
+
+/**
+ * Функция-обработчик события клик по таблице
+ *
+ * @param {MouseEvent} event
+ */
+function click(event) {
+    if (event.target.tagName !== 'TD') {
+        return;
+    }
+
+    if (event.shiftKey) {
+        event.target.style.backgroundColor = 'transparent';
+    } else {
+        event.target.style.backgroundColor = settings.backgroundColor;
+    }
+}
+
+
+/**
+ * Функция для генерации таблицы
+ *
+ * @param {HTMLTableElement} table
+ * @param {number} rowCount
+ * @param {number} columnCount
+ */
+function fillTable(table, rowCount = 50, columnCount= 50) {
+
+    for (let rowNumber = 0; rowNumber < rowCount; rowNumber++) {
+        let tr = document.createElement('tr');
+        for (let columnNumber = 0; columnNumber < columnCount; columnNumber++) {
+            let td = document.createElement('td');
+            tr.append(td);
+        }
+        table.append(tr);
+    }
+}
